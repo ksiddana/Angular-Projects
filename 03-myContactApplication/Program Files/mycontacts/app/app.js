e56b34c6e1b93cab6fcb347ee983ba4795bb28c1
Karun Siddana <ksiddana@gmail.com>
2016-03-22 09:16:55 -0700

    resolving extra files

--- 03-myContactApplication/Program Files/mycontacts/app/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Declare app level module which depends on views, and components
-angular.module('myContacts', [
-  'ngRoute'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/contacts'});
-}]);
